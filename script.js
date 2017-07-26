44a25209815e30691edc2ef28572a4f377fecea9
phytertek <ryan.phytertek@gmail.com>
2017-07-26 12:25:18 -0400

    ES6 conversion complete, added console.log statements for testing

--- script.js
+++ script.js
@@ -1,43 +1,68 @@
-var me = 'LambdaSchool';
+// var me = 'LambdaSchool';
+const me = 'LambdaSchool'
 
-var you = 'Student';
+// var you = 'Student';
+const you = 'Student'
 
-var numberOfCatsIOwn = 0;
+// var numberOfCatsIOwn = 0;
+let numberOfCatsIOwn = 0
 
-function buyCat() {
-  numberOfCatsIOwn++;
-  conosle.log('I now own ' + numberOfCatsIOwn + ' cats!');
+// function buyCat() {
+//   numberOfCatsIOwn++;
+//   conosle.log('I now own ' + numberOfCatsIOwn + ' cats!');
+// }
+const buyCat = () => {
+    numberOfCatsIOwn++
+    console.log(`I now own ${numberOfCatsIOwn} cats`)
+}
+buyCat()
+
+// var favoriteBooks = [
+//   'Captain Underpants',
+//   'Magic Treehouse',
+//   'Brown Bear, Brown Bear, What Do You See?',
+//   'Slaughterhouse 5',
+// ];
+const favoriteBooks = [
+    'Captain Underpants',
+    'Magic Treehouse',
+    'Brown Bear, Brown Bear, What Do You See?',
+    'Slaughterhouse 5',
+]
+
+// var likesCaptainUnderpants = function(bookList) {
+//   var yes = false;
+//   bookList.forEach(function(book) {
+//     if (book === 'Captain Underpants') yes = true;
+//   });
+//   return yes;
+// }
+const likesCaptainUnderpants = booklist => booklist.indexOf('Captain Underpants') >= 0
+
+// var result = likesCaptainUnderpants();
+const result = likesCaptainUnderpants
+console.log(result(favoriteBooks))
+
+// var sumInput = function() {
+//   var total = 0;
+//   for (var i = 0; i < arguments.length; i++) {
+//     total += arguments[i];
+//   }
+//   return total;
+// }
+function sumInput() {
+    return [...arguments].reduce((acc, av) => acc + av)
 }
 
-var favoriteBooks = [
-  'Captain Underpants',
-  'Magic Treehouse',
-  'Brown Bear, Brown Bear, What Do You See?',
-  'Slaughterhouse 5',
-];
+// var sum = sumInput(1, 2, 3, 4, 5);
+let sum = sumInput(1, 2, 3, 4, 5)
+console.log(sum)
 
-var likesCaptainUnderpants = function(bookList) {
-  var yes = false;
-  bookList.forEach(function(book) {
-    if (book === 'Captain Underpants') yes = true;
-  });
-  return yes;
-}
+// var add = function(x, x) {
+//   return x + x;
+// }
+const add = (x, y) => x + y
 
-var result = likesCaptainUnderpants();
-
-var sumInput = function() {
-  var total = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    total += arguments[i];
-  }
-  return total;
-}
-
-var sum = sumInput(1, 2, 3, 4, 5);
-
-var add = function(x, x) {
-  return x + x;
-}
-
-var sum = add(5, 5);
+// var sum = add(5, 5);
+sum = add(5, 5)
+console.log(sum)
